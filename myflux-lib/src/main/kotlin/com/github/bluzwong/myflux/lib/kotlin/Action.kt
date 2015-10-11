469d772c50f43bf1fa507cb1895bee6b47bfa77b
bruce.home <766796371@qq.com>
2015-10-11 15:24:02 +0800

    using bintray dependence

--- myflux-lib/src/main/kotlin/com/github/bluzwong/myflux/lib/kotlin/Action.kt
+++
@@ -1,8 +0,0 @@
-package com.github.bluzwong.myflux.lib.kotlin
-
-/**
- * Created by Bruce on 15/9/3.
- */
-public class Action(val type: String,val owner:Int, val dataMap: MutableMap<String, Any>) {
-    public fun getData(key: String): Any? = dataMap get key ?: null
-}
--- myflux-lib/src/main/kotlin/com/github/bluzwong/myflux/lib/kotlin/Response.kt
+++
@@ -1,9 +0,0 @@
-package com.github.bluzwong.myflux.lib.kotlin
-
-/**
- * Created by Bruce on 15/9/3.
- */
-public class Response(val type: String,val owner:Int, val dataMap: MutableMap<String, Any>) {
-    public fun getData(key: String): Any? = dataMap get key ?: null
-}
-//public fun new(type:String, dataMap:MutableMap<String, Any>):Response = Response(type, dataMap)
