2c6e38e09704beeed8eb3235bf6c9d034de2f166
bruce.home <766796371@qq.com>
2016-03-05 10:11:50 +0800

    VP内保持状态

--- app/src/main/java/com/github/bluzwang/myflux/example/DemoFragment.java
+++ app/src/main/java/com/github/bluzwang/myflux/example/DemoFragment.java
@@ -1,5 +1,6 @@
 package com.github.bluzwang.myflux.example;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -19,6 +20,11 @@ public class DemoFragment extends Fragment {
 
     private DemoFragmentRequester requester;
     TextView tv1,tv2,tv3,tv4;
+    String msg1,msg2,msg3,msg4;
+
+    public DemoFragment() {
+        setArguments(new Bundle());
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,14 +45,12 @@ public class DemoFragment extends Fragment {
         TextView tv = (TextView) view.findViewById(R.id.tv);
         tv.setText(new StringBuilder().append("this fragment => ").append(Integer.toHexString(this.hashCode())).append("\nrequester => ").append(Integer.toHexString(requester.hashCode())));
 
-
         view.findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 requester.requestSumIO(1, 2);
             }
         });
-
         view.findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,46 +58,85 @@ public class DemoFragment extends Fragment {
             }
         });
 
-
+        Bundle arguments = getArguments();
         if (savedInstanceState != null) {
-            String tv2Text = savedInstanceState.getString("tv2", "");
-            tv2.setText(tv2Text);
+           loadStates(savedInstanceState);
+        } else if (arguments != null) {
+            loadStates(arguments);
         }
         return view;
     }
 
+
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        Flux.fluxOnSaveInstanceState(requester, outState);
-        super.onSaveInstanceState(outState);
+    public void onPause() {
+        super.onPause();
+
     }
 
+
+    void loadStates(Bundle bundle) {
+        msg1 = bundle.getString("msg1", "not saved");
+        msg2 = bundle.getString("msg2", "not saved");
+        msg3 = bundle.getString("msg3", "not saved");
+        msg4 = bundle.getString("msg4", "not saved");
+        tv1.setText(msg1);
+        tv2.setText(msg2);
+        tv3.setText(msg3);
+        tv4.setText(msg4);
+    }
+    void saveStates() {
+        saveStates(getArguments());
+    }
+
+
+    void saveStates(Bundle bundle) {
+        bundle.putString("msg1",msg1);
+        bundle.putString("msg2",msg2);
+        bundle.putString("msg3",msg3);
+        bundle.putString("msg4",msg4);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Flux.fluxOnSaveInstanceState(requester, outState);
+        saveStates(outState);
+    }
 
     // 接收 type = "1" 的响应时运行
     @ReceiveType(type = {"1"})
     void doCcf(FluxResponse response) {
         int sum = (int) response.getData("sum");
-        tv1.setText("type = {\"1\"} => " + sum);
+        msg1 = "type = {\"1\"} => " + sum;
+        saveStates();
+        tv1.setText(msg1);
     }
 
     // 接收 type = "2" 的响应时运行
     @ReceiveType(type = "2")
     void doCcf2(FluxResponse response) {
         int sum = (int) response.getData("sum");
-        tv2.setText("type = \"2\" => " + sum);
+        msg2 = "type = \"2\" => " + sum;
+        saveStates();
+        tv2.setText(msg2);
     }
 
     // 接收 type = "2" 或者type = "2" 的响应时运行
     @ReceiveType(type = {"1", "2"})
     void dowsd2(FluxResponse response) {
         int sum = (int) response.getData("sum");
-        tv3.setText("type = {\"1\", \"2\"} => " + sum);
+        msg3 = "type = {\"1\", \"2\"} => " + sum;
+        saveStates();
+        tv3.setText(msg3);
     }
 
     // 接收 type = RequestType.REQUEST_1 或者type = RequestType.REQUEST_2 的响应时运行
     @ReceiveType(type = {RequestType.REQUEST_1, RequestType.REQUEST_2})
     void dowsd(FluxResponse response) {
         int sum = (int) response.getData("sum");
-        tv4.setText("type = {RequestType.REQUEST_1, RequestType.REQUEST_2} => " + sum);
+        msg4 = "type = {RequestType.REQUEST_1, RequestType.REQUEST_2} => " + sum;
+        saveStates();
+        tv4.setText(msg4);
     }
 }
